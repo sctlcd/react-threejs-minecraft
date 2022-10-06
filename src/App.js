361780779b4819ae9a87026c1c43566fe9ff98bc
lucinda <lucinda.souchet@gmail.com>
2022-10-06 17:41:48 +0200

    add Sky, Cloud, OrbitControls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Sky } from '@react-three/drei';
+import { Sky, Cloud, OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 
 import './App.css';
@@ -7,7 +7,21 @@ function App() {
   return (
     <>
       <Canvas>
-        <Sky />
+        <OrbitControls zoomSpeed={0.5}/> {/* define Orbit controls*/}
+        <Sky 
+          distance={450000}
+          sunPosition={[8, 1, 8]}
+          inclination={0}
+          azimuth={0.25}
+        /> {/* define Sky */}
+        <Cloud
+          args={[3, 2]}
+          opacity={0.1}
+          speed={0.4} // Rotation speed
+          width={10} // Width of the full cloud
+          depth={1.5} // Z-dir depth
+          segments={400} // Number of particles
+        /> {/* define Cloud */}
       </Canvas>
     </>
 
