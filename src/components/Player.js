a53c03130b1810bfba3a58ac3cb513379743189c
lucinda <lucinda.souchet@gmail.com>
2022-10-19 22:00:13 +0200

    jump when on the ground once in a row

--- src/components/Player.js
+++ src/components/Player.js
@@ -54,7 +54,7 @@ export const Player = () => {
     camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]));
     // api.velocity.set(0, 0, 0); // set velocity
 
-    if (actions.jump) {
+    if (actions.jump && Math.abs(vel.current[1]) < 0.01) {
       api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2]);
     }
   });
