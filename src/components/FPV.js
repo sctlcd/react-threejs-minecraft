c43bfb50de677f269bbdb1ee605952067253f3d4
lucinda <lucinda.souchet@gmail.com>
2022-10-21 11:39:28 +0200

    add comments

--- src/components/FPV.js
+++ src/components/FPV.js
@@ -1,8 +1,9 @@
 import { useThree } from '@react-three/fiber';
 import { PointerLockControls } from '@react-three/drei';
 
+// First Person View
 export const FPV = () => {
-  const { camera, gl } = useThree();
+  const { camera, gl } = useThree(); // define camera
 
   return (<PointerLockControls args={[camera, gl.domElement]} />);
 };
