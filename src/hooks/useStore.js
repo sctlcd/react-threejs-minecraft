5158bb5d3e6668b736b76dc6930fd503d6f12418
lucinda <lucinda.souchet@gmail.com>
2022-10-24 19:34:20 +0200

    render cube with various textures

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -7,11 +7,18 @@ export const useStore = create((set) => ({
   // texture
   texture: 'dirt',
   // array of cubes
-  cubes: [{
-    key: nanoid(),
-    pos: [ 0, 0.5, -3],
-    texture: 'dirt',
-  }],
+  cubes: [
+    {
+      key: nanoid(),
+      pos: [ 0, 0.5, -3],
+      texture: 'dirt',
+    },
+    {
+      key: nanoid(),
+      pos: [ 2, 0.5, -4],
+      texture: 'wood',
+    }
+  ],
   // methods to interact with the state 
   addCube: (x, y, z) => {
     set((prev) => ({
